9bf241af2f116093a37128d49e88e7f719b88488
Lillelund <alexander.lillelund@gmail.com>
2025-05-13 20:28:24 +0200

    added zig

--- modules/home-manager/starship/default.nix
+++ modules/home-manager/starship/default.nix
@@ -6,13 +6,27 @@
     enable = true;
     enableZshIntegration = true;
   };
-  programs.starship.settings = pkgs.lib.importTOML ./gruvbox-rainbow.toml;
   programs.starship.settings = {
+  "$schema" = "https://starship.rs/config-schema.json"
+   format = "[](color_orange)$os$username[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)$c$cpp$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:color_blue bg:color_bg3)$docker_context$conda$pixi[](fg:color_bg3 bg:color_bg1)$time[ ](fg:color_bg1)$line_break$character"
+palette = "gruvbox_dark"
     nix_shell = {
       symbol = "";
       format = "[$symbol$name]($style) ";
       style = "bright-purple bold";
       heuristic = true;
     };
+    palettes.gruvbox_dark = {
+		color_aqua = "#689d6a";
+		color_bg1 = "#3c3836";
+		color_bg3 = "#665c54";
+		color_blue = "#458588";
+		color_fg0 = "#fbf1c7";
+		color_green = "#98971a";
+		color_orange = "#d65d0e";
+		color_purple = "#b16286";
+		color_red = "#cc241d";
+		color_yellow = "#d79921";
+	};
   };
 }
---
+++ modules/home-manager/zig/default.nix
@@ -0,0 +1,5 @@
+{
+   programs.zig = {
+     enable = true;
+   };
+}
