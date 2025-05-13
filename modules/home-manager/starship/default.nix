cfdd0ae5b6f2c1c2930a9b265536360d9d92715e
Lillelund <alexander.lillelund@gmail.com>
2025-05-13 13:52:08 +0200

    added nix-shell to starship

--- modules/home-manager/starship/default.nix
+++ modules/home-manager/starship/default.nix
@@ -4,8 +4,15 @@
 }:{
   programs.starship = {
     enable = true;
-
     enableZshIntegration = true;
   };
   programs.starship.settings = pkgs.lib.importTOML ./gruvbox-rainbow.toml;
+  programs.starship.settings = {
+    nix_shell = {
+      symbol = "";
+      format = "[$symbol$name]($style) ";
+      style = "bright-purple bold";
+      heuristic = true;
+    };
+  };
 }
